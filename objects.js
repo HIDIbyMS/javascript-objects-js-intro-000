d62781fc0e47110993e46c4e15f6a91b56061a65
Michelle Sharpe <hidibyms@gmail.com>
2019-10-02 18:51:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,9 +4,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = "Def Leppard";
-  playlist.songTitle = "Pour Some Sugar on Me";
+  playlist['My Bloody Valentine'] = 'Sometimes'
 }
 
    
-      playlist['My Bloody Valentine'] = 'Sometimes'
