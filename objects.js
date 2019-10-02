a8adb1b56cb380da592fbf00cb2f4b0c5fae6ed8
Michelle Sharpe <hidibyms@gmail.com>
2019-10-02 20:09:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(obj, key, value) {
   return obj;
 }
 
-function removeFromPlaylist( [playlist], artistName){
+function removeFromPlaylist(){
   delete playlist.artistName;
   return playlist;
 }
